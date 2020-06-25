8eb236c8b291f3d50463b3730c4580949f50eeff
KabanFriends <yuukun2004@protonmail.ch>
2020-06-26 07:22:22 +0900

    起動画面の読込開始コードの変更

--- TJAPlayer3/Stages/01.StartUp/CStage起動.cs
+++ TJAPlayer3/Stages/01.StartUp/CStage起動.cs
@@ -75,24 +75,32 @@ namespace TJAPlayer3
 				base.OnManagedリソースの解放();
 			}
 		}
+
+		public void t読込開始()
+		{
+			bLoadStarted = true;
+
+			this.list進行文字列.Add(TJAPlayer3.AppDisplayNameWithInformationalVersion);
+			this.list進行文字列.Add("");
+			this.list進行文字列.Add("This is a fork of twopointzero/TJAPlayer3.");
+			this.list進行文字列.Add("");
+			this.list進行文字列.Add($"{TJAPlayer3.AppDisplayName} is open source software under the MIT license.");
+			this.list進行文字列.Add("See README for acknowledgments.");
+			this.list進行文字列.Add("");
+
+			es = new CEnumSongs();
+			es.StartEnumFromCache(); // 曲リスト取得(別スレッドで実行される)
+		}
+
 		public override int On進行描画()
 		{
 			if (!base.b活性化してない)
 			{
 				if (base.b初めての進行描画)
 				{
-					this.list進行文字列.Add(TJAPlayer3.AppDisplayNameWithInformationalVersion);
-					this.list進行文字列.Add("");
-					this.list進行文字列.Add("This is a fork of twopointzero/TJAPlayer3.");
-					this.list進行文字列.Add("");
-					this.list進行文字列.Add($"{TJAPlayer3.AppDisplayName} is open source software under the MIT license.");
-					this.list進行文字列.Add("See README for acknowledgments.");
-					this.list進行文字列.Add("");
-
-					es = new CEnumSongs();
-					es.StartEnumFromCache();                                        // 曲リスト取得(別スレッドで実行される)
+					this.t読込開始();
+					
 					base.b初めての進行描画 = false;
-					return 0;
 				}
 
 				// CSongs管理 s管理 = CDTXMania.Songs管理;
@@ -100,71 +108,74 @@ namespace TJAPlayer3
 				//if( this.tx背景 != null )
 				//	this.tx背景.t2D描画( CDTXMania.app.Device, 0, 0 );
 
-				#region [ this.str現在進行中 の決定 ]
-				//-----------------
-				switch (base.eフェーズID)
+				if (bLoadStarted)
 				{
-					case CStage.Eフェーズ.起動0_システムサウンドを構築:
-						this.str現在進行中 = "SYSTEM SOUND...";
-						break;
-
-					case CStage.Eフェーズ.起動00_songlistから曲リストを作成する:
-						this.str現在進行中 = "SONG LIST...";
-						break;
-
-					case CStage.Eフェーズ.起動1_SongsDBからスコアキャッシュを構築:
-						this.str現在進行中 = "SONG DATABASE...";
-						break;
-
-					case CStage.Eフェーズ.起動2_曲を検索してリストを作成する:
-						this.str現在進行中 = string.Format("{0} ... {1}", "Enumerating songs", es.Songs管理.n検索されたスコア数);
-						break;
-
-					case CStage.Eフェーズ.起動3_スコアキャッシュをリストに反映する:
-						this.str現在進行中 = string.Format("{0} ... {1}/{2}", "Loading score properties from songs.db", es.Songs管理.nスコアキャッシュから反映できたスコア数, es.Songs管理.n検索されたスコア数);
-						break;
-
-					case CStage.Eフェーズ.起動4_スコアキャッシュになかった曲をファイルから読み込んで反映する:
-						this.str現在進行中 = string.Format("{0} ... {1}/{2}", "Loading score properties from files", es.Songs管理.nファイルから反映できたスコア数, es.Songs管理.n検索されたスコア数 - es.Songs管理.nスコアキャッシュから反映できたスコア数);
-						break;
-
-					case CStage.Eフェーズ.起動5_曲リストへ後処理を適用する:
-						this.str現在進行中 = string.Format("{0} ... ", "Building songlists");
-						break;
-
-					case CStage.Eフェーズ.起動6_スコアキャッシュをSongsDBに出力する:
-						this.str現在進行中 = string.Format("{0} ... ", "Saving songs.db");
-						break;
-
-					case CStage.Eフェーズ.起動7_完了:
-						this.list進行文字列.Add("LOADING TEXTURES...");
-						TJAPlayer3.Tx.LoadTexture();
-						this.list進行文字列.Add("LOADING TEXTURES...OK");
-						this.str現在進行中 = "Setup done.";
-						break;
-				}
-				//-----------------
-				#endregion
-				#region [ this.list進行文字列＋this.現在進行中 の表示 ]
-				//-----------------
-				lock (this.list進行文字列)
-				{
-					int x = 320;
-					int y = 20;
-					foreach (string str in this.list進行文字列)
+					#region [ this.str現在進行中 の決定 ]
+					//-----------------
+					switch (base.eフェーズID)
 					{
-						TJAPlayer3.act文字コンソール.tPrint(x, y, C文字コンソール.Eフォント種別.白, str);
-						y += 24;
-					}
-					TJAPlayer3.act文字コンソール.tPrint(x, y, C文字コンソール.Eフォント種別.白, this.str現在進行中);
-				}
-				//-----------------
-				#endregion
+						case CStage.Eフェーズ.起動0_システムサウンドを構築:
+							this.str現在進行中 = "SYSTEM SOUND...";
+							break;
 
-				if (es != null && es.IsSongListEnumCompletelyDone)                          // 曲リスト作成が終わったら
-				{
-					TJAPlayer3.Songs管理 = (es != null) ? es.Songs管理 : null;      // 最後に、曲リストを拾い上げる
-					return 1;
+						case CStage.Eフェーズ.起動00_songlistから曲リストを作成する:
+							this.str現在進行中 = "SONG LIST...";
+							break;
+
+						case CStage.Eフェーズ.起動1_SongsDBからスコアキャッシュを構築:
+							this.str現在進行中 = "SONG DATABASE...";
+							break;
+
+						case CStage.Eフェーズ.起動2_曲を検索してリストを作成する:
+							this.str現在進行中 = string.Format("{0} ... {1}", "Enumerating songs", es.Songs管理.n検索されたスコア数);
+							break;
+
+						case CStage.Eフェーズ.起動3_スコアキャッシュをリストに反映する:
+							this.str現在進行中 = string.Format("{0} ... {1}/{2}", "Loading score properties from songs.db", es.Songs管理.nスコアキャッシュから反映できたスコア数, es.Songs管理.n検索されたスコア数);
+							break;
+
+						case CStage.Eフェーズ.起動4_スコアキャッシュになかった曲をファイルから読み込んで反映する:
+							this.str現在進行中 = string.Format("{0} ... {1}/{2}", "Loading score properties from files", es.Songs管理.nファイルから反映できたスコア数, es.Songs管理.n検索されたスコア数 - es.Songs管理.nスコアキャッシュから反映できたスコア数);
+							break;
+
+						case CStage.Eフェーズ.起動5_曲リストへ後処理を適用する:
+							this.str現在進行中 = string.Format("{0} ... ", "Building songlists");
+							break;
+
+						case CStage.Eフェーズ.起動6_スコアキャッシュをSongsDBに出力する:
+							this.str現在進行中 = string.Format("{0} ... ", "Saving songs.db");
+							break;
+
+						case CStage.Eフェーズ.起動7_完了:
+							this.list進行文字列.Add("LOADING TEXTURES...");
+							TJAPlayer3.Tx.LoadTexture();
+							this.list進行文字列.Add("LOADING TEXTURES...OK");
+							this.str現在進行中 = "Setup done.";
+							break;
+					}
+					//-----------------
+					#endregion
+					#region [ this.list進行文字列＋this.現在進行中 の表示 ]
+					//-----------------
+					lock (this.list進行文字列)
+					{
+						int x = 320;
+						int y = 20;
+						foreach (string str in this.list進行文字列)
+						{
+							TJAPlayer3.act文字コンソール.tPrint(x, y, C文字コンソール.Eフォント種別.白, str);
+							y += 24;
+						}
+						TJAPlayer3.act文字コンソール.tPrint(x, y, C文字コンソール.Eフォント種別.白, this.str現在進行中);
+					}
+					//-----------------
+					#endregion
+
+					if (es != null && es.IsSongListEnumCompletelyDone)                          // 曲リスト作成が終わったら
+					{
+						TJAPlayer3.Songs管理 = (es != null) ? es.Songs管理 : null;      // 最後に、曲リストを拾い上げる
+						return 1;
+					}
 				}
 			}
 			return 0;
@@ -178,6 +189,8 @@ namespace TJAPlayer3
 		private string str現在進行中 = "";
 		private CTexture tx背景;
 		private CEnumSongs es;
+
+		private bool bLoadStarted;
 		#endregion
 	}
 }
